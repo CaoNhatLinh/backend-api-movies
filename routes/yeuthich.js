a167568f3f550a46d98ac1854d954eed3e077d33
trieu2003 <120072804+trieu2003@users.noreply.github.com>
2024-06-09 19:36:34 +0700

    Update yeuthich.js

--- routes/yeuthich.js
+++ routes/yeuthich.js
@@ -2,42 +2,63 @@ const express = require('express');
 const router = express.Router();
 const db = require('../config/db');
 
-// Lấy danh sách tất cả các phim yêu thích
-router.get('/', (req, res) => {
-  db.query('SELECT * FROM YeuThich', (err, results) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-    } else {
-      res.json(results);
-    }
-  });
-});
+// // Lấy danh sách tất cả các phim yêu thích
+// router.get('/', (req, res) => {
+//   db.query('SELECT * FROM YeuThich', (err, results) => {
+//     if (err) {
+//       res.status(500).json({ error: err.message });
+//     } else {
+//       res.json(results);
+//     }
+//   });
+// });
 
-// Lấy danh sách phim yêu thích của người dùng theo ID người dùng
-router.get('/:maNguoiDung', (req, res) => {
-  const maNguoiDung = req.params.maNguoiDung;
-  db.query('SELECT * FROM YeuThich WHERE MaNguoiDung = ?', [maNguoiDung], (err, results) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-    } else {
-      res.json(results);
-    }
-  });
-});
+// // Lấy danh sách phim yêu thích của người dùng theo ID người dùng
+// router.get('/:maNguoiDung', (req, res) => {
+//   const maNguoiDung = req.params.maNguoiDung;
+//   db.query('SELECT * FROM YeuThich WHERE MaNguoiDung = ?', [maNguoiDung], (err, results) => {
+//     if (err) {
+//       res.status(500).json({ error: err.message });
+//     } else {
+//       res.json(results);
+//     }
+//   });
+// });
 
-// Thêm phim yêu thích mới cho người dùng
+// // Thêm phim yêu thích mới cho người dùng
+// router.post('/', (req, res) => {
+//   const yeuThich = req.body;
+//   db.query('INSERT INTO YeuThich SET ?', yeuThich, (err, result) => {
+//     if (err) {
+//       res.status(500).json({ error: err.message });
+//     } else {
+//       res.status(201).json({ ...yeuThich });
+//     }
+//   });
+// });
+
+// // Xóa phim yêu thích của người dùng
+// router.delete('/:maNguoiDung/:maPhim', (req, res) => {
+//   const maNguoiDung = req.params.maNguoiDung;
+//   const maPhim = req.params.maPhim;
+//   db.query('DELETE FROM YeuThich WHERE MaNguoiDung = ? AND MaPhim = ?', [maNguoiDung, maPhim], (err, result) => {
+//     if (err) {
+//       res.status(500).json({ error: err.message });
+//     } else {
+//       res.json({ message: 'Phim yêu thích đã được xóa' });
+//     }
+//   });
+// });
 router.post('/', (req, res) => {
-  const yeuThich = req.body;
-  db.query('INSERT INTO YeuThich SET ?', yeuThich, (err, result) => {
+  const { MaNguoiDung, MaPhim } = req.body;
+  db.query('INSERT INTO YeuThich (MaNguoiDung, MaPhim) VALUES (?, ?)', [MaNguoiDung, MaPhim], (err, result) => {
     if (err) {
       res.status(500).json({ error: err.message });
     } else {
-      res.status(201).json({ ...yeuThich });
+      res.status(201).json({ message: 'Phim đã được thêm vào danh sách yêu thích' });
     }
   });
 });
-
-// Xóa phim yêu thích của người dùng
 router.delete('/:maNguoiDung/:maPhim', (req, res) => {
   const maNguoiDung = req.params.maNguoiDung;
   const maPhim = req.params.maPhim;
@@ -49,5 +70,16 @@ router.delete('/:maNguoiDung/:maPhim', (req, res) => {
     }
   });
 });
+router.get('/:maNguoiDung', (req, res) => {
+  const maNguoiDung = req.params.maNguoiDung;
+  db.query('SELECT * FROM YeuThich WHERE MaNguoiDung = ?', [maNguoiDung], (err, results) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else {
+      res.json(results);
+    }
+  });
+});
+
 
 module.exports = router;
