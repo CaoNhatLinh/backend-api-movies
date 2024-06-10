5b4e71167ea868761be99e500e6dbddf7286ce96
BaoBao <dinhb140@gmail.com>
2024-06-10 11:23:40 +0700

    .

--- routes/danhgia.js
+++ routes/danhgia.js
@@ -30,19 +30,9 @@ router.get('/:id', (req, res) => {
 });
 
 // Thêm đánh giá mới
-// router.post('/ThemDanhGia', (req, res) => {
-//   const danhgia = req.body;
-//   db.query('INSERT INTO DanhGia SET ?', danhgia, (err, result) => {
-//     if (err) {
-//       res.status(500).json({ error: err.message });
-//     } else {
-//       res.status(201).json({ id: result.insertId, ...danhgia });
-//     }
-//   });
-// });
 router.post('/', (req, res) => {
-  const {Manguoidung,MapPhim, Danhgia,NgayDanhGia } = req.body;
-  db.query('INSERT INTO DanhGia(MaNguoiDung,MaPhim,DanhGia,Date) VALUES (?,?,?,?)', [Manguoidung,MapPhim,Danhgia,NgayDanhGia], (err, result) => {
+  const danhgia = req.body;
+  db.query('INSERT INTO DanhGia SET ?', danhgia, (err, result) => {
     if (err) {
       res.status(500).json({ error: err.message });
     } else {
@@ -50,6 +40,16 @@ router.post('/', (req, res) => {
     }
   });
 });
+// router.post('/', (req, res) => {
+//   const {Manguoidung,MapPhim, Danhgia,NgayDanhGia } = req.body;
+//   db.query('INSERT INTO DanhGia(MaNguoiDung,MaPhim,DanhGia,Date) VALUES (?,?,?,?)', [Manguoidung,MapPhim,Danhgia,NgayDanhGia], (err, result) => {
+//     if (err) {
+//       res.status(500).json({ error: err.message });
+//     } else {
+//       res.status(201).json({ id: result.insertId, ...danhgia });
+//     }
+//   });
+// });
 
 // Cập nhật thông tin đánh giá
 router.put('/:id', (req, res) => {
